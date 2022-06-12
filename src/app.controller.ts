8f70e0bb1c95686a3dfdb3d069106b5ef4f4a0f1
fabio brasil <fabiobfelix@hotmail.com>
2022-06-12 13:20:07 -0300

    fix rabbitmq conection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Logger, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
-import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices'
-import { Observable } from 'rxjs';
+import { ClientProxy, ClientProxyFactory, Payload, Transport } from '@nestjs/microservices'
+import { Observable, timeout } from 'rxjs';
 import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 
 
@@ -15,22 +15,31 @@ export class AppController {
     this.clientAdminBackend = ClientProxyFactory.create({
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://admim:admim@localhost:5672/srmartranking'],
-        queue: 'admin-backend'
+        urls: ['amqp://admin:admin@localhost:5672'],
+        queue: 'admin-backend',
+        // noAck: true,
+        queueOptions: {
+          durable: true
+        },
       },
     })
   }
 
-  @Post('categorias')
+  @Post('criacategoria')
   @UsePipes(ValidationPipe)
   async criarCategoria(
-    @Body() criarCategoriaDto: CriarCategoriaDto) {
+    // @Body() criarCategoriaDto: CriarCategoriaDto) {
+    @Body() criarCategoriaDto: any) {
+      console.log(criarCategoriaDto)
     return await this.clientAdminBackend.emit('criar-categoria', criarCategoriaDto)
   }
 
-  @Get('categoria/:id')
-  consultarCategorias(@Query('idCategoria') _id: string): Observable<any>{
+  @Get('categoria/teste')
+  consultarCategorias(@Payload() data: any):Observable<any> {
+    let message = this.clientAdminBackend.send('consultar-categorias', data);
 
-    return this.clientAdminBackend.send('consultar-categorias', _id ? _id: '')
+    // JSON.stringify(message)
+    console.log(message)
+    return message
   }
 } 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 
 
 @Module({
-  imports: [],
+  imports: [AppController,ClientsModule],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
